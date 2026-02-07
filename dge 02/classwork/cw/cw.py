910eac8df4129fbfa84c16abdfdc70b8b67ea2fa
abiashvililuka9-cell <abiashvililuka9@gmail.com>
2026-02-07 16:03:44 +0400

    second commit

---
+++ dge 02/classwork/cw/cw.py
@@ -0,0 +1,24 @@
+# + ოპერატორი ერთმანეთს უმატებს inger_ებს და float_ებს 
+print(2+2)
+
+# - ოპერატორი აკლებს ერთმანეთს integer_ებს და float_ებს
+print(3-1)
+
+# % ოპერატორს გამოაქვს ნაშთი
+print(3 % 2)
+
+# / ოპერატორი ყოფს integer_ებს და float_ებს და გამოაქვს float_ის სახით
+print(4 / 2)
+
+# // ოპერატორი ყოფს integer_ებს და float_ებს integer_ის შემთხვევაში გამოაქვს integer_ის სახით და float_ის დროს float სახით
+print(2 // 2)
+print(3.10 // 2.05)
+
+#concantation იგივე კონკატინაცია ატყუპებს ცვლადებს 
+name = 'luka'
+surname = 'abiashvili'
+print(name + surname)
+
+num1 = 15
+num2 = 20
+print(num1 + num2)
