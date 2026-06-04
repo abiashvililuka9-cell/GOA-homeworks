9dacda60a863c3bd1297fd036acb693ae8c19923
abiashvililuka9-cell <abiashvililuka9@gmail.com>
2026-06-04 20:59:38 +0400

    second commit

---
+++ day032/homework/main.py
@@ -0,0 +1,40 @@
+# tuple1 = (1, 2, 3)
+# a, b, c = tuple1
+# print(a)
+# print(b)
+# print(c)
+
+
+
+# count, len, max, min, sorted, sum, list
+
+
+
+
+# append, remove, insert, pop
+
+
+
+
+# info = ('luka', '16', 'gldani', 'lobio', 'rame')
+# name, age, mis, sakvebi, rame = info
+# print(name)
+# print(age)
+# print(mis)
+# print(sakvebi)
+# print(rame)
+
+
+
+
+
+# tuple2 = (1, 2, 3, 4.5, )
+
+
+
+
+list1 = 'a', 'b', 'g'
+print(' '.join(list1))
+
+list2 = 'a b g'
+print(list2.split())
